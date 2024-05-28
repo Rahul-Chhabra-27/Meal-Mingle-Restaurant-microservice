5b9f85e68d9cbc5d6552785921700708977231c0
Rahul Chhabra <chhabrarahul2707@gmail.com>
2024-05-28 22:25:29 +0530

    Proto file added.

---
+++ generate.sh
@@ -0,0 +1,8 @@
+go get -u gorm.io/gorm
+go get -u gorm.io/driver/mysql
+go get -u google.golang.org/grpc
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
+go get github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway
+go get github.com/grpc-ecosystem/grpc-gateway/v2/runtime
+go get github.com/joho/godotenv
