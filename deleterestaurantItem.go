270309090f03cf039a021492648098d2865a03c6
Rahul Chhabra <chhabrarahul2707@gmail.com>
2024-06-05 19:21:26 +0530

    delete restaurant item added.

--- deleterestaurantItem.go
+++ deleterestaurantItem.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"restaurant-micro/model"
 	restaurantpb "restaurant-micro/proto/restaurant"
+	"strings"
 
 	"google.golang.org/grpc/codes"
 )
@@ -19,6 +21,43 @@ func (*RestaurantService) DeleteRestaurantItem(ctx context.Context, response *re
 			Error:      "Internal Server Error",
 		}, nil
 	}
-	fmt.Println(userEmail);
-	return nil,nil;
+	fmt.Println(userEmail)
+	restaurantName := strings.ReplaceAll(response.RestaurantName, "-", " ")
+	restaurantItemName := strings.ReplaceAll(response.RestaurantItemName, "-", " ")
+
+	// check if user own's this restaurant
+	var restaurant model.Restaurant
+	primaryKeyRes := restaurantDBConnector.Where("name = ?", restaurantName).First(&restaurant)
+	if primaryKeyRes.Error != nil || restaurant.RestaurantOwnerMail != userEmail {
+		return &restaurantpb.DeleteRestaurantItemResponse{
+			Message:    "",
+			StatusCode: int64(codes.NotFound),
+			Error:      "Restaurant Does not exist OR you are not the owner of this restaurant",
+		}, nil
+	}
+
+	// delete the restaurant item
+	var restaurantItem model.RestaurantItem
+	primaryKey := restaurantItemDBConnector.Where("item_name = ? AND restaurant_id = ?", restaurantItemName, restaurant.ID).First(&restaurantItem)
+	if primaryKey.Error != nil {
+		return &restaurantpb.DeleteRestaurantItemResponse{
+			Message:    "",
+			StatusCode: int64(codes.Internal),
+			Error:      "Restaurant item does not exist",
+		}, nil
+	}
+	err := restaurantItemDBConnector.Delete(&restaurantItem)
+	if err.Error != nil {
+		return &restaurantpb.DeleteRestaurantItemResponse{
+			Message:    "",
+			StatusCode: 500,
+			Error:      "Failed to delete restaurant item",
+		}, nil
+	}
+	return &restaurantpb.DeleteRestaurantItemResponse{
+		Message:    "Restaurant item deleted successfully",
+		StatusCode: int64(codes.OK),
+		Error:      "",
+	}, nil
 }
+
